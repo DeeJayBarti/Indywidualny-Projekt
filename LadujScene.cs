85d030e78ccea53919184901a7528a558136b83a
DeeJayBarti <borowski.bartek@wp.pl>
2018-04-10 18:26:41 +0200

    Add files via upload

---
+++ LadujScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadujScene : MonoBehaviour {
+
+	// Use this for initialization
+	public void ZaladujScene (int nrSceny) {
+        Application.LoadLevel (nrSceny);
+	}
+	
+	// Update is called once per frame
+	public void Wyjscie () {
+        Application.Quit();
+	}
+}
