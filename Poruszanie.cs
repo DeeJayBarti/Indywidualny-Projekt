cc5e1a49c0e69e12a4eac67c263cd7075da6b987
Bartłomiej <borowski.bartek@wp.pl>
2018-05-10 20:39:45 +0200

    Add files via upload

---
+++ Poruszanie.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poruszanie : MonoBehaviour {
+
+    public float PredkoscPoruszania = 0.5f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PoruszaniePrawo();
+	}
+
+    void PoruszaniePrawo()
+    {
+        if (gameObject.CompareTag("Friend"))
+        {
+            transform.Translate(Vector2.right * PredkoscPoruszania * Time.deltaTime);
+        }
+        else {
+            transform.Translate(Vector2.right * -PredkoscPoruszania * Time.deltaTime);
+        }
+        //transform.Translate(Vector2.right * PredkoscPoruszania * Time.deltaTime);
+    }
+}
